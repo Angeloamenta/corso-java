dda8cad1614830af6656aa1b8936f378730fbae8
Angeloamenta <Angeloamenta7@gmail.com>
2025-02-13 12:39:44 +0100

    overloades

---
+++ Overloaded.java
@@ -0,0 +1,21 @@
+// gli Overloaded metod sono metodi che hanno lo stesso nome ma con firme diverse.
+// più metodi con lo stesso nome ma con firme diverse.
+// metodi uguali ma l'importante è che i parametri siano numrericamente diversi o tipi diversi
+
+public class Overloaded {
+    public static void main(String[] args) {
+
+
+    }
+
+    static int addizione(int a, int b) {
+        return a + b;
+    }
+
+    static double addizione(double a, double b) {
+        return a + b;
+    }
+
+
+
+}
